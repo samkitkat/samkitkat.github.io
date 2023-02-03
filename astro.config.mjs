2a0fc8e90f6f3e7707e2d72a7ca902e45a66a85f
Samar Barakat <samarbarakat23@gmail.com>
2023-02-02 16:31:12 -0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://samkitkat.github.io',
   integrations: [mdx(), sitemap()]
 });
